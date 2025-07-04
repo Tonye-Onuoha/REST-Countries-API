7b1c624a0e333e61eb673e06387e6798384f5cc4
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-07-05 00:33:36 +0100

    Created initial styling for "Details" component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { ThemeIsLightContext } from "./ThemeContext";
 
-export default function Input() {
+export default function Input({ onSearch, onFilter }) {
     const { themeIsLight } = useContext(ThemeIsLightContext);
 
     return (
@@ -10,17 +10,19 @@ export default function Input() {
                 className={themeIsLight ? "light-theme-element" : "dark-theme-element"}
                 type="text"
                 placeholder="Search for a country..."
+                onChange={onSearch}
             />
             <select
                 className={themeIsLight ? "light-theme-element" : "dark-theme-element"}
                 name="region"
                 id="region"
-                defaultValue="filter">
+                defaultValue="filter"
+                onChange={onFilter}>
                 <option value="filter" disabled>
                     Filter by Region
                 </option>
                 <option value="africa">Africa</option>
-                <option value="america">America</option>
+                <option value="americas">Americas</option>
                 <option value="asia">Asia</option>
                 <option value="europe">Europe</option>
                 <option value="oceania">Oceania</option>
