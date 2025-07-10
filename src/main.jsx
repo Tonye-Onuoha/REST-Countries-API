95efef883af3eb9c3a79474ff09f44c9f3beb0aa
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-07-10 22:27:32 +0100

    Added routing configurations to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import CountryDetails from "./pages/CountryDetails";
+import countryLoader from "./utils/countryLoader";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "details/:countryName", Component: CountryDetails, loader: countryLoader }
+    ]
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
